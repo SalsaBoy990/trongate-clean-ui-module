7f8daf8231df2058716c0208f0fa1fda0ed327bc
Gulácsi András <guland@protonmail.com>
2024-03-15 23:24:47 +0100

    Clean.UI minor updates & fixes release v1.0.1
    - The initially-opened accordion should have the minus icon
    - Add screen-reader-only (.sr-only) class,
    - .bar-item width and display should not change on mobile (<=550px), also true for ".prev-next navigation"
    - no-bullets list should have left-paddings (useful when it comes to embedded lists / hierarchy; use ".padding-left-0" when you need it)
    - Scrollbar thickness changes now at <=600px breakpoint
    - table of content: limit max-width (breaks longer content)
    - Mark deprecated UI components (lightbox, tabbed images, and slideshow)
    - Add the missing "breadcrumb-left" class on the demo page
    - Change button padding, line height to normal

--- ui/clean/views/index.php
+++ ui/clean/views/index.php
@@ -86,7 +86,7 @@
 
             <button @click="toggleAccordion('accordionOne')" aria-controls="accordionOne" type="button"
                     class="fs-16 flex left-align accordion-button">
-                <span>Open Section 1</span><i class="fa-solid fa-plus"></i>
+                <span>Open Section 1</span><i class="fa-solid fa-minus"></i>
             </button>
             <div id="accordionOne" :aria-expanded="isAccordionContentExpanded('accordionOne')"
                  class="hide accordion-item show">
@@ -425,7 +425,7 @@
 
         <p>Left-aligned breadcrumb</p>
 
-        <nav class="breadcrumb">
+        <nav class="breadcrumb breadcrumb-left">
             <ol>
                 <li>
                     <a href="#">
@@ -1002,7 +1002,7 @@
         <hr>
 
 
-        <h3>14. Lightbox</h3>
+        <h3>14. Lightbox (deprecated)</h3>
         <p>Combine <a href="#">Modals</a> and <a href="#">Slideshows</a> to create a
             lightbox
             (modal image gallery):</p>
@@ -1270,7 +1270,7 @@
         <hr>
 
 
-        <h3>20. Slideshow</h3>
+        <h3>20. Slideshow (deprecated)</h3>
         <p>Clean.ui provide <a href="#">slideshows</a> for cycling through images or
             other
             content:</p>
@@ -1362,7 +1362,7 @@
 
         <br>
 
-        <p>Tabbed Image Gallery (Click on one of the pictures):</p>
+        <p>Tabbed Image Gallery (Click on one of the pictures) (deprecated):</p>
 
         <div x-data="tabbedImagesData">
             <div class="row-padding">
